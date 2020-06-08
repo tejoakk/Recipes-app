7a22dbe73edba190879a6d2ffb2b0d392d0d2677
Tejo Akkala <tejo.akkala@gmail.com>
2020-06-08 14:57:34 +0100

    project structure

---
+++ app/src/main/java/com/teo/recipes/App.kt
@@ -0,0 +1,29 @@
+package com.teo.recipes
+
+import android.app.Activity
+import android.app.Application
+import com.teo.recipes.di.AppInjector
+import com.teo.recipes.util.CrashReportingTree
+import com.facebook.stetho.Stetho
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
+import timber.log.Timber
+import javax.inject.Inject
+
+class App : Application(), HasActivityInjector {
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Activity>
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) Stetho.initializeWithDefaults(this)
+
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+        else Timber.plant(CrashReportingTree())
+
+        AppInjector.init(this)
+    }
+
+    override fun activityInjector() = dispatchingAndroidInjector
+}
